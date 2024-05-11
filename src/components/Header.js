78e701ec2daf5cac4db8bb0e117bfd30e79a3f32
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2024-05-11 10:51:21 +0530

    Created one sample component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Header extends React.Component{
+
+    render(){
+        return (
+
+            <>
+            Hello
+            </>
+
+        );
+    }
+
+}
