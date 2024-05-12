f46de6c37b2ca0887e2e07424fbe1c43391a5ceb
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2024-05-12 16:27:37 +0530

    Finalized left side of about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,39 @@
 import React from "react";
+import logoDark from '../assets/logo-for-dark.png';
+import avatar from '../assets/avatar.png';
+import './About.css';
+import { Card } from 'primereact/card';
+
 
 export default class About extends React.Component {
 
     render() {
 
         return (
-            <div className="container-about">
+            <div className="container-about mt-6 mb-6">
                 <div className="grid">
-                    <div className="col">
-                        Vinamra Shrey
+                    <div className="col-5 col-offset-1 pl-8 pr-8 mt-6">
+                        <div class="container border-round-3xl h-30rem">
+                            <div class="align-content-evenly border-round-2xl flex flex-column glass-card h-30rem  mb-8">
+                                <img
+                                    src={avatar}
+                                    alt="logo"
+                                    loading="lazy"
+                                    className="h-14rem"
+                                />
+
+                                <img
+                                    src={logoDark}
+                                    alt="logo"
+                                    loading="lazy"
+                                    className="h-8rem mt-6"
+                                />
+                            </div>
+                        </div>
+                        <div class="circle1"></div>
+                        <div class="circle2"></div>
                     </div>
-                    <div className="col flex justify-content-around">
+                    <div className="col-6">
                         <div>Home</div>
                         <div>About</div>
                         <div>Skills</div>
@@ -18,34 +41,10 @@ export default class About extends React.Component {
                         <div>Projects</div>
                         <div>Contact</div>
                     </div>
+
+
                 </div>
-                <div className="grid">
-                    <div className="col-7">
-                        <p className="">Test Lines</p> 
-                    </div>
-                    <div className="col-5">
-                        <div className="grid">
-                            <div className="col">
-                                <div className="">5</div>
-                                <div className="">Projects completed</div>
-                            </div>
-                            <div className="col">
-                                <div className="">5</div>
-                                <div className="">Worked in fortune</div>
-                            </div>
-                        </div>
-                        <div className="grid">
-                            <div className="col">
-                                <div className="">5+</div>
-                                <div className="">Years experience</div>
-                            </div>
-                            <div className="col">
-                                <div className="">5</div>
-                                <div className="">Technologies worked</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         );
     }
