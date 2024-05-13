e77943a66c78c8ff3ec7b8a61c2ea33673559b25
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2024-05-13 20:16:47 +0530

    Removed warning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 import f1 from '../assets/footer/f1.jpg';
 import f2 from '../assets/footer/f2.svg';
 import f3 from '../assets/footer/f3.svg';
-import f4 from '../assets/footer/f4.svg';
 import f5 from '../assets/footer/f5.svg';
 import f6 from '../assets/footer/f6.svg';
 
@@ -15,7 +14,6 @@ export default class Footer extends React.Component {
                 <img className="h-13rem ml-7"  src={f2} alt="footer" loading="lazy" />
                 <img className="h-13rem relative"  src={f5} alt="footer" loading="lazy" style={{left: "-2rem"}} />
                 <img className="h-13rem"  src={f3} alt="footer" loading="lazy" />
-                {/* <img className="h-13rem"  src={f4} alt="footer" loading="lazy" /> */}
                 <img className="h-13rem"  src={f6} alt="footer" loading="lazy" />
             </div>
         );
