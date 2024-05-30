73ff0f0a7bc1f8ae3d0b04588a5e4f54bef03213
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2024-05-30 13:48:27 +0530

    Optimized footer and contact for mobile view

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,22 +3,20 @@ import { Button } from 'primereact/button';
 
 export default class Contact extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="align-content-center h-25rem">
                 <div className="align-items-center flex flex-column gap-5">
-                    <div className="font-bold text-5xl text-800 text-center">Let`s work together !</div>
+                    <div className="font-bold md:text-5xl text-2xl text-800 text-center">Let`s work together !</div>
                     <div className="line-height-3 text-700 text-center text-xl"
                         style={{
-                            width: "40rem",
+                            // width: "40rem",
                             letterSpacing: "0.03rem"
                         }}>
                         Let's team up and build something great. Send me a message to get started.</div>
-                    <Button label="Contact Me" size="large" className="font-bold pb-3 pl-5 pr-5 pt-3 w-fit"
+                    <Button label="Contact Me" size="large" className="font-bold pb-3 pl-5 pr-5 pt-3 w-fit hidden md:block"
+                    onClick={(() => this.props.toggleDialog(true))} />
+                    <Button label="Contact Me" size="small" className="font-bold pb-3 pl-5 pr-5 pt-3 w-fit md:hidden block"
                     onClick={(() => this.props.toggleDialog(true))} />
                 </div>
             </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,11 +10,11 @@ export default class Footer extends React.Component {
     render() {
         return (
             <div className="flex justify-content-between">
-                <img className="h-13rem"  src={f1} alt="footer" loading="lazy" />
-                <img className="h-13rem ml-7"  src={f2} alt="footer" loading="lazy" />
-                <img className="h-13rem relative"  src={f5} alt="footer" loading="lazy" style={{left: "-2rem"}} />
-                <img className="h-13rem"  src={f3} alt="footer" loading="lazy" />
-                <img className="h-13rem"  src={f6} alt="footer" loading="lazy" />
+                <img className="h-13rem m-auto"  src={f1} alt="footer" loading="lazy" />
+                <img className="h-13rem ml-7 hidden md:block"  src={f2} alt="footer" loading="lazy" />
+                <img className="h-13rem relative hidden md:block"  src={f5} alt="footer" loading="lazy" style={{left: "-2rem"}} />
+                <img className="h-13rem hidden md:block"  src={f3} alt="footer" loading="lazy" />
+                <img className="h-13rem hidden md:block"  src={f6} alt="footer" loading="lazy" />
             </div>
         );
     }
