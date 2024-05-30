11cd2c0f8258b70ff307cb573b4c946277f30799
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2024-05-30 13:59:04 +0530

    Optimized skills for mobile view

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -21,17 +21,17 @@ export default class Skills extends React.Component {
     render() {
         return (
             <div id="skills" className="container-skills pb-6 pt-6" style={{ backgroundColor: "#2d333b" }}>
-                <div className="font-bold text-5xl text-center" style={{ color: "#00ca5a" }}>Technologies used</div>
-                <div className="flex flex-wrap gap-5 justify-content-evenly mt-6 pl-8 pr-8">
+                <div className="font-bold md:text-5xl text-2xl text-center" style={{ color: "#00ca5a" }}>Technologies used</div>
+                <div className="flex flex-wrap gap-4 justify-content-evenly md:gap-5 md:mt-6 md:pl-8 md:pr-8 mt-5 pl-3 pr-3">
                     {this.state.techImages?.map((image, index) => {
                         const match = image.match(/\/media\/([^\.]+)/);
                         return (
                             <div key={"skill-body-"+index} className="fadeinleft animation-duration-500 animation-ease-in-out">
-                                <img className="h-5rem"
+                                <img className="h-3rem md:h-5rem"
                                     key={"skill-image-"+index} src={image} alt="info"
                                     title={match ? match[1] : null}
                                     loading="lazy" />
-                                <div key={"skill-name-"+index} className="capitalize font-bold mt-3 text-200 text-sm" style={{
+                                <div key={"skill-name-"+index} className="capitalize md:font-bold md:mt-3 md:text-sm mt-2 text-200 text-xs hidden md:block" style={{
                                     letterSpacing: "0.08rem"
                                 }}>{match ? match[1]?.replaceAll("-", " ") : null}</div>
                             </div>
