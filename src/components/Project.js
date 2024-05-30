ec4fcc751f533efa12c500c03b2e3e9733220fcc
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2024-05-31 02:10:06 +0530

    Hit and trial for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,29 +12,30 @@ export default class Project extends React.Component {
                 key={"project-" + index}
                 header={
                     <img
-                        className="h-14rem w-20rem border-round-top-md"
+                        className="h-8rem md:h-14rem w-12rem md:w-20rem border-round-top-md"
                         src={require(`../assets/project/${project.image}`)} alt="Project Images"
                         loading="lazy" />
                 }
-                className="p-card p-component w-fit shadow-5 border-round-bottom-md mb-6 ml-2 mt-4"
-                style={{ height: "33rem" }}>
-                <div className="flex flex-column gap-2 pl-2 pr-2 text-left">
-                    <div className="font-medium text-600 text-xl">
+                className="md:w-fit md:shadow-5 border-round-bottom-md md:mb-6 md:ml-2 md:mt-4 mt-2"
+                //style={{ height: "33rem" }}
+                >
+                <div className="flex flex-column gap-2 md:pl-2 md:pr-2 text-left">
+                    <div className="font-medium text-600 md:text-xl text-sm">
                         {project.type}
                     </div>
-                    <div className="capitalize font-bold text-3xl">
+                    <div className="capitalize font-bold md:text-3xl text-xl">
                         {project.name}
                     </div>
-                    <p className="text-600 h-4rem w-20rem">
+                    <p className="text-600 md:h-4rem md:w-20rem">
                         {project.description}
                     </p>
                     {
                         project.link ? 
-                        <a href={project.link} target="_blank" rel="noopener noreferrer" className="font-bold p-button pb-3 pt-3 w-fit"
+                        <a href={project.link} target="_blank" rel="noopener noreferrer" className="font-bold p-button md:pb-3 md:pt-3 w-fit"
                         style={{ textDecoration: "none" }}>
                         Explore more
                     </a>:
-                    <Button label="No preview available" disabled severity="secondary" className="font-bold pb-3 pt-3 w-fit"/>
+                    <Button label="No preview available" disabled severity="secondary" className="font-bold md:pb-3 md:pt-3 w-fit"/>
                     }
                     
                 </div>
@@ -79,15 +80,15 @@ export default class Project extends React.Component {
 
         return (
             <div  id="projects" className="" style={containerStyle}>
-                <div className="flex flex-column h-full justify-content-evenly pl-8 pr-8 pt-8">
-                    <div className="font-bold text-5xl text-left" style={{ color: "#00ca5a" }}>Projects</div>
-                    <div className="font-medium text-600 text-xl text-left">
-                        <p className="line-height-2 text-justify"
+                <div className="flex flex-column h-full justify-content-evenly md:pl-8 md:pr-8 md:pt-8 mt-4">
+                    <div className="font-bold md:text-5xl text-2xl text-left" style={{ color: "#00ca5a" }}>Projects</div>
+                    <div className="font-medium text-600 md:text-xl text-left">
+                        <p className="line-height-3 text-justify"
                             style={{ letterSpacing: "0.04rem" }}>
                             Here are some projects I have worked on, alongside various enterprise and personal endeavours. They are mostly hosted on my home server to keep costs low.</p>
                     </div>
                     <Carousel value={projects} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions}
-                        className="custom-carousel mt-3"
+                        className="custom-carousel md:mt-3 md:mt-1"
                         circular
                         itemTemplate={this.projectTemplate} />
                 </div>
