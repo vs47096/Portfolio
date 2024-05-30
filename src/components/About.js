d87b05abfc8792fd4e3e10c7c586b6e9e58abd1c
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2024-05-31 01:18:42 +0530

    Optimized  about me for mobile view

--- src/components/About.js
+++ src/components/About.js
@@ -15,36 +15,35 @@ export default class About extends React.Component {
         return (
             <div id="about" className="container-about mt-6 mb-6">
                 <div className="grid">
-                    <div className="col-5 col-offset-1 pl-8 pr-8 mt-6" style={{ height: "32rem" }}>
+                    <div className="col md:col-5 md:col-offset-1 md:mt-6 md:pl-8 md:pr-8 mt-4 pl-5 pr-5" style={{ height: "32rem" }}>
                         <div className="container border-round-3xl h-30rem">
-                            <div className="align-content-evenly border-round-2xl flex flex-column glass-card h-30rem  mb-8 shadow-5">
+                            <div className="align-content-evenly border-round-2xl flex flex-column glass-card h-30rem  mb-8 shadow-5 w-fit">
                                 <img
                                     src={avatar}
                                     alt="logo"
                                     loading="lazy"
-                                    className="h-14rem"
+                                    className="h-10rem md:h-14rem"
                                 />
 
                                 <img
                                     src={logoDark}
                                     alt="logo"
                                     loading="lazy"
-                                    className="h-8rem mt-6"
+                                    className="h-7rem md:h-8rem mt-6"
                                 />
                             </div>
                         </div>
                         <div className="circle1"></div>
                         <div className="circle2"></div>
                     </div>
-                    <div className="align-content-center col-5 text-left">
-                        <div className="font-bold text-700"><p className="text-4xl"><span className="underline-highlight-green">ABOUT ME</span></p></div>
+                    <div className="align-content-center md:col-5 text-left md:pl-0 md:pr-0 pl-5 pr-5">
+                        <div className="font-bold text-700"><p className="text-xl md:text-4xl"><span className="underline-highlight-green">ABOUT ME</span></p></div>
                         <div>
-                            <p className="font-medium mb-6 text-700 text-justify text-xl line-height-2"
-                                style={{letterSpacing: "0.05rem"}}>
+                            <p className="font-medium md:mb-6 text-700 text-justify md:text-xl line-height-3 about-me-content">
                             Hey there! I'm someone who finds joy in both Coding and Exploring the world. I'm on a quest to debug the world, one destination at a time. Alongside, I am committed to continuous learning, particularly in the realm of Machine learning, where I actively engage in project development. As an Engineer, I possess a comprehensive understanding of System Architectures, delving into Network and Security aspects to ensure end-to-end functionality.
                             </p>
                         </div>
-                        <Button label="Let`s connect" size="large" className="font-bold pb-3 pl-5 pr-5 pt-3"
+                        <Button label="Let`s connect" size="large" className="font-bold pb-3 pl-5 pr-5 pt-3 hidden md:block"
                         onClick={(() => this.props.toggleDialog(true))}/>
                     </div>
 
