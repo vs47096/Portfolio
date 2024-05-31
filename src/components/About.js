ca1f2e54495cbc735546c6dce6f8e9d96b9ab97d
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2024-05-31 20:00:20 +0530

    Removed extra padding from right

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ export default class About extends React.Component {
 
         return (
             <div id="about" className="container-about mt-6 md:mb-6 mb-4">
-                <div className="grid">
+                <div className="grid w-full">
                     <div className="col md:col-5 md:col-offset-1 md:mt-6 md:pl-8 md:pr-8 mt-4 pl-5 pr-5" style={{ height: "32rem" }}>
                         <div className="container border-round-3xl h-30rem">
                             <div className="align-content-evenly border-round-2xl flex flex-column glass-card h-30rem  mb-8 shadow-5 w-fit">
